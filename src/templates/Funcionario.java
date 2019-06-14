6838599c40b9e0e950afa4e1cf7737be1133d7fd
Lucas <lukkass.pereira@hotmail.com>
2019-06-14 03:02:58 -0300

    Getters e Setters Funcionarios

--- src/templates/Funcionario.java
+++ src/templates/Funcionario.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package templates;
 
-/**
- *
- * @author userseven
- */
 public class Funcionario {
     
+   private String Nome, BatePonto;
+   private int Idade, Codigo, Cpf;
+   
+    public Funcionario (String Nome,String BatePonto, int Idade, int Codigo, int Cpf ){
+        this.Nome = Nome;
+        this.BatePonto = BatePonto;
+        this.Idade = Idade;
+        this.Cpf = Cpf;
+    }
+    
+    public String getNome () {
+        return Nome;
+    }
+    
+    public void setNome (String Nome) {
+        this.Nome = Nome;
+    }
+    
+    public String getBatePonto(){
+        return BatePonto;
+    }
+    
+    public void setBatePonto (String BatePonto){
+        this.BatePonto = BatePonto;
+    }
+    
+    public int getIdade (){
+        return Idade;
+    }
+    
+    public void setIdade (int Idade){
+        this.Idade = Idade;
+    }
+    
+    public int getCpf () {
+        return Cpf;
+    }
+    
+    public void setCpf (int Cpf) {
+        this.Cpf = Cpf;
+    }
+   
 }
