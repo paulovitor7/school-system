6886652a8642c5139e19b7ac8c24d1401e599164
Lucas <lukkass.pereira@hotmail.com>
2019-06-15 19:22:19 -0300

    Private turma e toString em Aluno

--- src/actors/Aluno.java
+++ src/actors/Aluno.java
@@ -4,10 +4,12 @@ import templates.Pessoa;
 
 public class Aluno extends Pessoa{
     private static int ra;
+    private String turma;
     
-    public Aluno(String nome, String cpf, int idade) {
+    public Aluno(String nome, String cpf, int idade, String turma) {
         super(nome, cpf, idade);
         
+        this.turma = turma;
         ra++;
     }
     
@@ -15,5 +17,25 @@ public class Aluno extends Pessoa{
         
         return ra;
     }
+    
+    public String getTurma (){
+        return  this.turma;
+    }
+    
+    public void setTurma (String turma) {
+        this.turma = turma;
+    }
 
+    
+    @Override
+    public String toString () { 
+            return "\n\nAluno: "+this.ra+"\n"+
+                
+                "Nome: "+nome+"\n"+
+                "CPF: "+cpf+"\n"+
+                "Idade: "+idade+"\n"+
+                "n/n Turma: "+this.turma;
+    
+                
+     }
 }
