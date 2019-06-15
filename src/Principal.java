f5f06db896c158a4b18bbd8eaf1cdffe6d5c9cbd
Paulo Vitor <pvitorp321@gmail.com>
2019-06-14 22:28:30 -0300

    Crando Array do tipo Professor

--- src/Principal.java
+++ src/Principal.java
@@ -5,39 +5,46 @@ import java.util.Scanner;
 public class Principal {
     static Scanner scan = new Scanner(System.in);
     static Global g = new Global();
-    static boolean sair = true;
-    
+    static boolean sair = false;
+    static final int MAXPROF=2, MAXALUNO=20;
     public static void main(String[] args){
+        Professor[] prof = new Professor[MAXPROF];
+
         g.about();
         
-        while(sair){
+        while(!sair){
             g.menuPrincipal();
             int opc = scan.nextInt();
             switch(opc){
                 case 1:
-                    professor();
+                    professor(prof);
                     break;
                 case 2:
                     aluno();
                     break;
                 case 3:
                     System.out.println("\nVocê escolheu sair.\nFinalizando programa...");
-                    sair = false;
+                    sair = true;
                     break;
                 default:
                     System.out.println("\nNenhuma das opções foi selecionada.\nFinalizando programa...");
-                    sair = false;
+                    sair = true;
                     break;
             }
         }
         scan.close();
     }
     
-    private static void professor(){
-        System.out.print("\n< Professor >");
-        g.subMenu();
-        int opc = scan.nextInt();
-        switch(opc){
+    private static void professor(Professor prof[]){
+        System.out.print("\n< Professor >\n\nMáximo de cadastro: "+MAXPROF);
+        boolean sairProf = false;
+
+        while(!sairProf){
+            g.subMenu();
+            int opc = scan.nextInt();
+            scan.nextLine();
+            
+            switch(opc){
             case 1:
                 System.out.println("Add");
                 break;
@@ -52,15 +59,19 @@ public class Principal {
                 break;
             case 5:
                 System.out.println("\nVoltando...");
+                sairProf = true;
                 break;
             case 6:
                 System.out.println("\nVocê escolheu sair.\nFinalizando programa...");
-                sair = false;
+                sairProf = true;
+                sair = true;
                 break;
             default:
                 System.out.println("\nNenhuma das opções foi selecionada.\nFinalizando programa...");
-                sair = false;
+                sairProf = true;
+                sair = true;
                 break;
+            }
         }
     }
     
